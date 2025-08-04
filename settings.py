7dab16b372ed33c7bbf200b5caeac9a94eef67ac
Pierlab <plab71@hotmail.com>
2025-08-04 23:43:47 +0200

    avec les bons réglages

--- settings.py
+++ settings.py
@@ -5,16 +5,16 @@
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 720
-TICK_DURATION = 20  # ms (pour accélérer la journée à 3 minutes)
-NUM_VILLAGERS = 10
+TICK_DURATION = 50  # ms (pour accélérer la journée à 3 minutes)
+NUM_VILLAGERS = 30
 
 # Vitesse de déplacement des villageois (en pixels par tick, calculée pour 5 km/h)
-KMH_TO_PIXELS_PER_TICK = (500 * 1000 / 60 / 60) * (SCREEN_WIDTH / 40000)
+KMH_TO_PIXELS_PER_TICK = (5 * 1000 / 60 / 60) * (SCREEN_WIDTH / 500)
 
 # Facteurs de déplacement
-MOVEMENT_RANDOM_FACTOR = 0.1  # Intensité de l'aléatoire lors des déplacements
-NEAR_DESTINATION_RADIUS = 3  # Rayon (en px) de déplacement autour de la cible
+MOVEMENT_RANDOM_FACTOR = 10  # Intensité de l'aléatoire lors des déplacements
+NEAR_DESTINATION_RADIUS = 30  # Rayon (en px) de déplacement autour de la cible
 
 # Tailles des personnages selon l'âge
-ADULT_RADIUS = 12
-CHILD_RADIUS = 8
+ADULT_RADIUS = 15
+CHILD_RADIUS = 6
